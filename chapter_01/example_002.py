5a8304d01f5ae0a8c1ee4041ec58fbf841b4f844
Selfimpr <yuchen352416@163.com>
2019-09-23 12:56:12 +0800

    init

---
+++ chapter_01/example_002.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def removeDuplicates(nums: list) -> int:
+
+    indexs = []
+    i = 0
+    while nums.__len__() - 1 > i:
+        if nums[i] == nums[i + 1]:
+            indexs.append(i)
+        i = i + 1
+
+    i = indexs.__len__() - 1
+    while i >= 0:
+        nums.pop(indexs[i])
+        i = i - 1
+
+    print(indexs)
+    print(nums)
+    return nums.__len__()
+
+
+
+if __name__ == '__main__':
+    arr = [0, 0, 1, 1, 1, 2, 3, 4]
+    print(arr)
+    l = removeDuplicates(arr)
+    print(l)
+
+
+
