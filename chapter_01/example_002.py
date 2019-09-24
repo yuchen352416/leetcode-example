9e58a4ff44d7cccd142996ca70f72036cf1498a5
Selfimpr <yuchen352416@163.com>
2019-09-24 10:06:33 +0800

    添加测试数据

--- chapter_01/example_002.py
+++ chapter_01/example_002.py
@@ -12,10 +12,13 @@ def maxProfit(prices: list) -> int:
 
 
 if __name__ == '__main__':
-    arr = [7,1,5,3,6,4]
+    # arr = [7, 1, 5, 3, 6, 4]
+    # arr = [6, 1, 3, 2, 4, 7]
+    # arr = [1, 2, 3, 4, 5]
+    arr = [3, 3, 5, 0, 0, 3, 1, 4]
     print(arr)
-    l = maxProfit(arr)
-    print(l)
+    p = maxProfit(arr)
+    print(p)
 
 
 
