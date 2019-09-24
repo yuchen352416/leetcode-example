3a5d973af3bf7275b760a2537275b29c6431f6af
Selfimpr <yuchen352416@163.com>
2019-09-24 09:38:59 +0800

    添加买卖股票的最佳时机2算法

--- chapter_01/example_002.py
+++ chapter_01/example_002.py
@@ -2,13 +2,17 @@
 
 # 买卖股票的最佳时机2
 def maxProfit(prices: list) -> int:
-
-    return 0
+    if prices.__len__() < 2: return 0
+    sum = 0
+    for i in range(prices.__len__() - 1):
+        if prices[i] < prices[i + 1]:
+            sum += prices[i + 1] - prices[i]
+    return sum
 
 
 
 if __name__ == '__main__':
-    arr = []
+    arr = [7,1,5,3,6,4]
     print(arr)
     l = maxProfit(arr)
     print(l)
