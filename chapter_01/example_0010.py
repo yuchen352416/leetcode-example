24d3916768797eed7a0d034e2bc59b4fa3117283
Selfimpr <yuchen352416@163.com>
2019-09-28 21:15:44 +0800

    刷题~

---
+++ chapter_01/example_0010.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# 有效的数独
+def isValidSudoku(nums: list) -> list:
+
+    return nums
+
+if __name__ == '__main__':
+    arr = [0, 0, 1]
+    print(arr)
+    result = isValidSudoku(arr)
+    print(result)
