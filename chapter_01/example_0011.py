ce631d55edb28433c4afbf77f18dc186d8387464
Selfimpr <yuchen352416@163.com>
2019-10-07 18:46:56 +0800

    刷题~

---
+++ chapter_01/example_0011.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+# 旋转图像
+def isValidSudoku(board: list) -> bool:
+    return True
+
+
+if __name__ == '__main__':
+    arr = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+    print(arr)
+    result = isValidSudoku(arr)
+    print(result)
---
+++ chapter_02/example_0001.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# 反转字符串
+def reverseString(s: list) -> None:
+    l = s.__len__()
+    for i in range(l // 2):
+        s[i], s[l - i - 1] = s[l - i - 1], s[i]
+
+
+
+if __name__ == '__main__':
+    arr = ["h", "e", "l", "l", "o"]
+    print(arr)
+    reverseString(arr)
+    print(arr)
