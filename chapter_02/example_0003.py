66295c0058e44d720a16e8dbb0cf1ec34b7d9854
Selfimpr <yuchen352416@163.com>
2019-11-05 22:06:04 +0800

    刷题~

--- chapter_02/example_0003.py
+++ chapter_02/example_0003.py
@@ -2,10 +2,33 @@
 
 # 字符串中的第一个唯一字符
 def firstUniqChar(s: str) -> int:
-    pass
+
+    # container = {}
+    # if len(s) == 0:
+    #     return -1
+    # elif len(s) == 1:
+    #     return 0
+    # else:
+    #     for i in range(len(s)):
+    #         if container.__contains__(s[i]):
+    #             container[s[i]] += 1
+    #         else:
+    #             container[s[i]] = 0
+    #     for key in container.keys():
+    #         if container.get(key) == 0:
+    #             return s.index(key)
+    # return -1
+
+    dic = {}
+    for s1 in s:
+        dic[s1] = dic.get(s1, 0) + 1
+    for k, v in dic.items():
+        if v == 1:
+            return s.index(k)
+    return -1
 
 
 
 if __name__ == '__main__':
-    result = firstUniqChar('leetcode')
+    result = firstUniqChar("left")
     print(result)
---
+++ chapter_02/example_0004.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# 有效的字母异位词
+def isAnagram(s: str, t: str) -> bool:
+    dictA = {}
+    dictB = {}
+    if len(s) != len(t):
+        return False
+    else:
+        for i in range(len(s)):
+            dictA[s[i]] = dictA.get(s[i], 0) + 1
+            dictB[t[i]] = dictB.get(t[i], 0) + 1
+        for k, v in dictA.items():
+            if v != dictB.get(k, 0):
+                return False
+        return True
+
+
+
+if __name__ == '__main__':
+    result = isAnagram("anagram", "nagaram")
+    print(result)
