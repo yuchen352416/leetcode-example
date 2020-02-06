95aa23e5e74242d06b507c9a4a2dc747f2bfa9ea
Selfimpr <yuchen352416@163.com>
2020-02-06 20:46:10 +0800

    new add issue

---
+++ brush_topic/issue_0001.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from lib.ListLibraries import ListNode, ListInitialize
+
+
+if __name__ == '__main__':
+    node = ListInitialize([1, 2, 3]).getNode()
+    print(node.val)
+
+
+
+
+
