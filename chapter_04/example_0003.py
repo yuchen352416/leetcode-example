ca400a26935c3bcce15d642ead69485b86bccdd1
Selfimpr <yuchen352416@163.com>
2020-03-07 21:17:47 +0800

    刷题~

--- chapter_04/example_0003.py
+++ chapter_04/example_0003.py
@@ -1,14 +1,55 @@
 #!/usr/bin/python3
 
 from lib.ListLibraries import TreeNode, TreeNodeInitialize
-from typing import List
 
 
 class Solution:
 
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        pass
+    def isSymmetric(self, root: TreeNode) -> bool:
+        """
+        type root: TreeNode
+        rtype bool
+        """
+        if root is None or (root.left is None and root.right is None):
+            return True
+        if root.left is None or root.right is None:
+            return False
+        left = root.left
+        right = root.right
+        if left.val != right.val:
+            return False
+        return self.doublePointer(left, right)
 
+    def doublePointer(self, left: TreeNode, right: TreeNode) -> bool:
+        """
+        type left: TreeNode
+        type right: TreeNode
+        rtype bool
+        """
+        # 外层判断
+        if left.left is not None and right.right is not None:
+            flag = self.doublePointer(left.left, right.right)
+            if not flag or not left.val == right.val:
+                return False
+
+        elif left.left is None and right.right is None:
+            flag = left.val == right.val
+            if not flag:
+                return False
+        else:
+            return False
+        # 内层判断
+        if left.right is not None and right.left is not None:
+            flag = self.doublePointer(left.right, right.left)
+            if not flag or not left.val == right.val:
+                return False
+        elif left.right is None and right.left is None:
+            flag = left.val == right.val
+            if not flag:
+                return False
+        else:
+            return False
+        return True
 
 
 if __name__ == '__main__':
--- chapter_04/example_0004.py
+++ chapter_04/example_0004.py
@@ -1,55 +1,18 @@
 #!/usr/bin/python3
 
 from lib.ListLibraries import TreeNode, TreeNodeInitialize
+from typing import List
 
 
 class Solution:
 
-    def isSymmetric(self, root: TreeNode) -> bool:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
         """
         type root: TreeNode
-        rtype bool
+        rtype List
         """
-        if root is None or (root.left is None and root.right is None):
-            return True
-        if root.left is None or root.right is None:
-            return False
-        left = root.left
-        right = root.right
-        if left.val != right.val:
-            return False
-        return self.doublePointer(left, right)
+        pass
 
-    def doublePointer(self, left: TreeNode, right: TreeNode) -> bool:
-        """
-        type left: TreeNode
-        type right: TreeNode
-        rtype bool
-        """
-        # 外层判断
-        if left.left is not None and right.right is not None:
-            flag = self.doublePointer(left.left, right.right)
-            if not flag or not left.val == right.val:
-                return False
-
-        elif left.left is None and right.right is None:
-            flag = left.val == right.val
-            if not flag:
-                return False
-        else:
-            return False
-        # 内层判断
-        if left.right is not None and right.left is not None:
-            flag = self.doublePointer(left.right, right.left)
-            if not flag or not left.val == right.val:
-                return False
-        elif left.right is None and right.left is None:
-            flag = left.val == right.val
-            if not flag:
-                return False
-        else:
-            return False
-        return True
 
 
 if __name__ == '__main__':
