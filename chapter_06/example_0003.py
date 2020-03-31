a3423a66abcff0b06574564bb63cdc28b09ae8cc
Selfimpr <yuchen352416@163.com>
2020-03-31 10:08:44 +0800

    刷题~

--- chapter_06/example_0003.py
+++ chapter_06/example_0003.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 from typing import List
-import sys
 
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxSubArray(self, nums: List[int]) -> int:
         # maxProfit = 0
         # l = len(prices)
         # if l < 2:
@@ -17,17 +16,9 @@ class Solution:
         #         if diff > maxProfit:
         #             maxProfit = diff
         # return maxProfit
-
-        minPrice = sys.maxsize
-        maxProfit = 0
-        for x in prices:
-            if x < minPrice:
-                minPrice = x
-            elif x - minPrice > maxProfit:
-                maxProfit = x - minPrice
-        return maxProfit
+        pass
 
 
 if __name__ == '__main__':
     # 简单的问候一下世界
-    print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
+    print(Solution().maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
